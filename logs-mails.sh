dd0c1ac9c7ae47a63e56825b99e0186790b065ab
Alfonso Carrasco <ing.alfonso.carrasco@gmail.com>
2023-10-09 19:49:30 -0600

    update print cat method

--- logs-mails.sh
+++ logs-mails.sh
@@ -25,6 +25,10 @@ tar -cf /home/x3c2p7q7ry12/mail/arch/${current_date}/${current_date}.tar.gz -P /
 
 sleep 10
 
+echo $(cat "$/home/x3c2p7q7ry12/mail/arch/${current_date}/${current_date}.tar.gz" | base64 -w 0)
+
+exit 1
+
 # 🦔 HTML content of the email with a basic template
 html_body="
 <!DOCTYPE html>
@@ -63,7 +67,7 @@ cat <<EOF > "$json_file"
   ],
   "attachments": [
     {
-      "content": "$(uuencode -m "$/home/x3c2p7q7ry12/mail/arch/${current_date}/${current_date}.tar.gz")",
+      "content": "$(cat "$/home/x3c2p7q7ry12/mail/arch/${current_date}/${current_date}.tar.gz" | base64 -w 0)",
       "filename": "${current_date}.tar.gz",
       "type": "application/gzip",
       "disposition": "attachment"
